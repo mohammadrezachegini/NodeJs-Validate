197394af80eeb383d2a88173eb313a23d066a223
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-03 09:31:59 -0800

    add register schema for validation

---
+++ validators/auth.validator.js
@@ -0,0 +1,23 @@
+const {default: Schema} = require("validate")
+
+
+const registerSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        length: {min: 4, max: 20}
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /[a-z0-9]\_\.]{5,50}@[a-z]{2,10}.[a-z]{2,10}/gi
+    },
+    role: {
+        type: String,
+        enum: ['user','admin','teacher']
+    }
+})
+
+module.exports = {
+    registerSchema
+}
