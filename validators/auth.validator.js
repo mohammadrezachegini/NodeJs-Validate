0766e5551749f344b439ea88c422a19ebfd575ca
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-03 09:51:39 -0800

    final commit

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -5,12 +5,21 @@ const registerSchema = new Schema({
     username: {
         type: String,
         required: true,
-        length: {min: 4, max: 20}
+        length: {min: 4, max: 20},
+        message: {
+            length:"username is less than 4 characters"
+           
+        }
     },
     email: {
         type: String,
         required: true,
-        match: /[a-z0-9]\_\.]{5,50}@[a-z]{2,10}.[a-z]{2,10}/gi
+        match: /[a-z0-9]\_\.]{5,50}@[a-z]{2,10}.[a-z]{2,10}/gi,
+        message: {
+            type:"email must to be string",
+            match: "email format is invalid",
+            required:"email is required"
+        }
     },
     role: {
         type: String,
